25ee3df2acbec8b7d5d7556b78c3b358efd9f12d
John Lynch <johnlynch.32768@gmail.com>
2019-03-17 01:58:36 +0000

    Set up testing, incl. one failing test running successfully

---
+++ stone/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+class TestDjango(TestCase):
+    """docstring for TestDjango"""
+    def test_works(self):
+        self.assertEqual(0, 1)
+
+
+        
