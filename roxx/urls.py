aecfd5e207329ac1a757313616b98cc1307d0afe
John Lynch <johnlynch.32768@gmail.com>
2019-03-18 01:48:10 +0000

    Modified routes in urls.py files to send base URL to forest().

--- roxx/urls.py
+++ roxx/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('stone.urls'), name = "testurl"),
     path('stone/', include('stone.urls'), name = "testurl"),
 ]
--- stone/urls.py
+++ stone/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.forest, name = 'forest'),
     path('forest', views.forest, name = 'forest'),
     path('addtree', views.addtree, name = 'addtree'),
     path('edittree/<int:tree_id>', views.edittree, name = 'edittree')
